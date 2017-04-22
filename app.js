4bfb415302614a5a0388679840eed1581759da64
Joe Spedale <jspedale@gmail.com>
2017-04-22 00:10:38 -0400

    Added final score and changed pure event outcomes  to descriptions.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const month = 'month_04';
-const day = 'day_18';
-const year = 'year_2017';
+const month = '04';
+const day = '18';
+const year = '2017';
 const mets = 'nynmlb';
 const opponent = 'phimlb';
-const full_date = '2017_04_18';
+const full_date = `${year}_${month}_${day}`;
 
-const boxScore = fetch(`http://gd2.mlb.com/components/game/mlb/${year}/${month}/${day}/gid_${full_date}_${opponent}_${mets}_1/boxscore.json`)
+const boxScore = fetch(`http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/gid_${full_date}_${opponent}_${mets}_1/boxscore.json`)
 let bottomOrTop;
   boxScore
     .then(data => data.json())
@@ -17,13 +17,23 @@ let bottomOrTop;
         else bottomOrTop = 'top';
         let scoreDiv = document.querySelector('.score');
         if (bottomOrTop === 'bottom') {
-          console.log(data.data.boxscore.linescore.home_team_runs)
-          scoreDiv.innerHTML = `<div>The Mets scored ${data.data.boxscore.linescore.home_team_runs} runs`
+          scoreDiv.innerHTML = `
+            <div>Final score</div>
+            <div>New York Mets: ${data.data.boxscore.linescore.home_team_runs}</div>
+            <div>${data.data.boxscore.away_fname}: ${data.data.boxscore.linescore.away_team_runs}</div>
+          `
+        }
+        else if (bottomOrTop === 'top') {
+          scoreDiv.innerHTML = `
+            <div>Final score</div>
+            <div>New York Mets: ${data.data.boxscore.linescore.away_team_runs}</div>
+            <div>${data.data.boxscore.home_fname}: ${data.data.boxscore.linescore.home_team_runs}</div>
+          `
         }
     })
     .catch(err => console.error(err));
 
-const gameEvent = fetch('http://gd2.mlb.com/components/game/mlb/year_2017/month_04/day_18/gid_2017_04_18_phimlb_nynmlb_1/game_events.json')
+const gameEvent = fetch(`http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/gid_${full_date}_${opponent}_${mets}_1/game_events.json`)
 gameEvent
   .then(data => data.json())
   .then(data => data.data.game.inning.map(e => e[bottomOrTop].atbat))
@@ -31,8 +41,8 @@ gameEvent
     console.log(at_bats_per_inning_bottom)
     return at_bats_per_inning_bottom.map(at_bat_appearances => {
       return at_bat_appearances.map(at_bat_appearance => {
-        console.log(at_bat_appearance.event)
-        return at_bat_appearance.event
+        console.log(at_bat_appearance.des)
+        return at_bat_appearance.des
       })
     })
   })
