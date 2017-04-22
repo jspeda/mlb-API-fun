caeb4f4be16bc26cd1e61504750257b220080f7e
Joe Spedale <jspedale@gmail.com>
2017-04-22 00:56:42 -0400

    Added jQuery and user can now access any mets home game by adjusting the variables in the top of the JS file.

--- app.js
+++ app.js
@@ -1,69 +1,74 @@
-const month = '04';
-const day = '18';
-const year = '2017';
-const mets = 'nynmlb';
-const opponent = 'phimlb';
-const full_date = `${year}_${month}_${day}`;
+$(document).ready(function() {
+  const month = '04';
+  const day = '21';
+  const year = '2017';
+  const mets = 'nynmlb';
+  const opponent = 'wasmlb';
+  const fullDate = `${year}_${month}_${day}`;
 
-const boxScore = fetch(`http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/gid_${full_date}_${opponent}_${mets}_1/boxscore.json`)
-let bottomOrTop;
-  boxScore
+  // NOTE: THIS ONLY CURRENTLY WORKS FOR METS HOME GAMES DUE TO URL STRUCTURE - SHOULD BE AN EASY FIX
+
+  const boxScore = fetch(`http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/gid_${fullDate}_${opponent}_${mets}_1/boxscore.json`)
+  let bottomOrTop;
+    boxScore
+      .then(data => data.json())
+      .then(data => {
+        console.log(data.data.boxscore.home_fname)
+          if (data.data.boxscore.home_fname === 'New York Mets') {
+            bottomOrTop = 'bottom';
+          }
+          else bottomOrTop = 'top';
+          if (bottomOrTop === 'bottom') {
+            var opposingTeam = data.data.boxscore.away_fname;
+            $('.score').html(`
+              <div>Final score</div>
+              <div>New York Mets: ${data.data.boxscore.linescore.home_team_runs}</div>
+              <div>${data.data.boxscore.away_fname}: ${data.data.boxscore.linescore.away_team_runs}</div>
+            `)
+          }
+          else if (bottomOrTop === 'top') {
+            var opposingTeam = data.data.boxscore.home_fname;
+            $('.score').html(`
+              <div>Final score</div>
+              <div>New York Mets: ${data.data.boxscore.linescore.away_team_runs}</div>
+              <div>${data.data.boxscore.home_fname}: ${data.data.boxscore.linescore.home_team_runs}</div>
+            `)
+          }
+          $('.title').html(`<div class="title">Outcome of every New York Mets at bat on ${fullDate} vs ${opposingTeam}</div>`)
+      })
+      .catch(err => console.error(err));
+
+  const gameEvent = fetch(`http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/gid_${fullDate}_${opponent}_${mets}_1/game_events.json`)
+  gameEvent
     .then(data => data.json())
-    .then(data => {
-      console.log(data.data.boxscore.home_fname)
-        if (data.data.boxscore.home_fname === 'New York Mets') {
-          bottomOrTop = 'bottom';
-        }
-        else bottomOrTop = 'top';
-        let scoreDiv = document.querySelector('.score');
-        if (bottomOrTop === 'bottom') {
-          scoreDiv.innerHTML = `
-            <div>Final score</div>
-            <div>New York Mets: ${data.data.boxscore.linescore.home_team_runs}</div>
-            <div>${data.data.boxscore.away_fname}: ${data.data.boxscore.linescore.away_team_runs}</div>
-          `
-        }
-        else if (bottomOrTop === 'top') {
-          scoreDiv.innerHTML = `
-            <div>Final score</div>
-            <div>New York Mets: ${data.data.boxscore.linescore.away_team_runs}</div>
-            <div>${data.data.boxscore.home_fname}: ${data.data.boxscore.linescore.home_team_runs}</div>
-          `
-        }
-    })
-    .catch(err => console.error(err));
-
-const gameEvent = fetch(`http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/gid_${full_date}_${opponent}_${mets}_1/game_events.json`)
-gameEvent
-  .then(data => data.json())
-  .then(data => data.data.game.inning.map(e => e[bottomOrTop].atbat))
-  .then(at_bats_per_inning_bottom => {
-    console.log(at_bats_per_inning_bottom)
-    return at_bats_per_inning_bottom.map(at_bat_appearances => {
-      return at_bat_appearances.map(at_bat_appearance => {
-        console.log(at_bat_appearance.des)
-        return at_bat_appearance.des
+    .then(data => data.data.game.inning.map(e => e[bottomOrTop].atbat))
+    .then(at_bats_per_inning_bottom => {
+      console.log(at_bats_per_inning_bottom)
+      return at_bats_per_inning_bottom.map(at_bat_appearances => {
+        return at_bat_appearances.map(at_bat_appearance => {
+          console.log(at_bat_appearance.des)
+          return at_bat_appearance.des
+        })
       })
     })
-  })
-  .then(events_per_inning => {
-    Object.keys(events_per_inning).forEach(inning => {
-      events_per_inning[inning].unshift(`<h5>inning ${Number(inning) + 1}</h5>`)
-    });
-    return events_per_inning.reduce((events, event) => {
-      return events.concat(event)
-    }, []);
-  })
-  .then(events => {
-    console.log('events', events)
-    let div = document.getElementById('list');
-    let strikeouts = events.filter(e => {
-      if (e === "Strikeout") return true;
+    .then(events_per_inning => {
+      Object.keys(events_per_inning).forEach(inning => {
+        events_per_inning[inning].unshift(`<h5>inning ${Number(inning) + 1}</h5>`)
+      });
+      return events_per_inning.reduce((events, event) => {
+        return events.concat(event)
+      }, []);
     })
-    events.map(e => {
-      div.innerHTML += `<div class="batter-events">${e}</div>`
+    .then(events => {
+      console.log('events', events)
+      let strikeouts = events.filter(e => {
+        if (e === "Strikeout") return true;
+      })
+      events.map(e => {
+        $('.list').append(`<div class="batter-events">${e}</div>`);
+      })
     })
-  })
-  .catch(err => {
-    console.error(err);
-  })
+    .catch(err => {
+      console.error(err);
+    })
+})
