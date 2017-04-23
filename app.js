f8e0a924561fa8fe91379e1c38843248b5c2ffb1
Joe Spedale <jspedale@gmail.com>
2017-04-22 23:25:13 -0400

    Removed extraneous comment and adjusted variable names for increased clarity.

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ $(document).ready(function() {
     `http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/gid_${fullDate}_${opponent}_${mets}_1` :
     `http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/gid_${fullDate}_${mets}_${opponent}_1`
 
-  // NOTE: THIS ONLY CURRENTLY WORKS FOR METS HOME GAMES DUE TO URL STRUCTURE - SHOULD BE AN EASY FIX
-
-  const boxScore = fetch(`${url}/boxscore.json`)
+  const boxScore = fetch(`${url}/boxscore.json`);
   let bottomOrTop;
     boxScore
       .then(data => data.json())
@@ -37,24 +35,24 @@ $(document).ready(function() {
               <div>Final score</div>
               <div>New York Mets: ${data.data.boxscore.linescore.away_team_runs}</div>
               <div>${data.data.boxscore.home_fname}: ${data.data.boxscore.linescore.home_team_runs}</div>
-            `)
+            `);
           }
           $('.title').html(`<div class="title">Outcome of every New York Mets at bat on ${fullDate} vs ${opposingTeam}</div>`)
       })
       .catch(err => console.error(err));
 
-  const gameEvent = fetch(`${url}/game_events.json`)
+  const gameEvent = fetch(`${url}/game_events.json`);
   gameEvent
     .then(data => data.json())
     .then(data => data.data.game.inning.map(e => e[bottomOrTop].atbat))
-    .then(at_bats_per_inning_bottom => {
-      console.log(at_bats_per_inning_bottom)
-      return at_bats_per_inning_bottom.map(at_bat_appearances => {
+    .then(at_bats_per_inning => {
+      console.log(at_bats_per_inning)
+      return at_bats_per_inning.map(at_bat_appearances => {
         return at_bat_appearances.map(at_bat_appearance => {
           console.log(at_bat_appearance.des)
           return at_bat_appearance.des
-        })
-      })
+        });
+      });
     })
     .then(events_per_inning => {
       Object.keys(events_per_inning).forEach(inning => {
